22ba0e3647112b1f711627d136378f33a1990cd7
Birardo <j.verderi@edu.senai.br>
2026-08-28 08:34:09 -0300

    Adicionar arquivos de projeto.

---
+++ Carro.cs
@@ -0,0 +1,22 @@
+﻿namespace abstracao
+{
+    public class Carro
+    {
+        //atributos (características)
+        public string Marca;
+        public string Modelo;
+        public int Ano;
+        public string Cor;
+
+        //metodos (comportamento)
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"Carro : {Marca} {Modelo}");
+            Console.WriteLine($"Cor : {Cor}, Ano: {Ano}");
+            Console.WriteLine("-------------------------");
+        }
+
+
+
+    }
+}
---
+++ Produto.cs
@@ -0,0 +1,16 @@
+﻿namespace abstracao
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int QuantidadeEstoque;
+
+        public void ExibirInformacoesProduto()
+        {
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Preço : {Preco}\nQuantidade no estoque: {QuantidadeEstoque}");
+            Console.WriteLine("-------------------------");
+        }
+    }
+}
